7f1cf5e96f34776c55fda858f968bafdbd8177e1
Ivan <cryptox3@gmail.com>
2012-04-27 10:07:48 +0300

    Update controllers/Site.php

--- controllers/Site.php
+++ controllers/Site.php
@@ -1,8 +1,13 @@
 <?php
 class Site
 {
-    public static function news()
+    public static function news($matches)
     {
-        echo 123;
+        $page = isset($matches[1]) ? abs($matches[1]) : 1;
+        $perPage = Process::$context['cms']['news']['limit_per_page'];
+        $pagination = Data::paginate(Database::count('news'), $perPage, $page);
+        Process::$context['news_list'] = News::listing($pagination['offset'], $perPage);
+        Process::$context['pagination'] = ($pagination['total_pages'] > 1) ? $pagination : false;
     }
 }
+
