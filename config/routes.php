730fb20af9e08138d80f91879e333fb73cbd8b29
Ivan <cryptox3@gmail.com>
2012-04-27 10:03:45 +0300

    Update config/routes.php

--- config/routes.php
+++ config/routes.php
@@ -6,6 +6,6 @@
  * if route first symbol is ! - this is controller (!class:method)
  */
 return array(
-    '/' => array('!site:news', 'homepage'),
+    '/(?:index\.html|page_(\d+)\.html)?' => array('!site:news', 'homepage'),
     '/test' => '!common:test',
 );
