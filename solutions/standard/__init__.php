8eb82a89754c305ee9d20c2fda120a9098172c2f
Student Ivan <cryptox3@gmail.com>
2013-08-10 23:28:00 +0400

    admin dev 10.08.2013 11:25 PM fix3

--- solutions/standard/__init__.php
+++ solutions/standard/__init__.php
@@ -26,7 +26,7 @@ if (isset($config['cms']))
 	
 	require_once $__DIRADM__ . '/datamappers/Stats.php';
 	
-	if ($_SERVER['REMOTE_ADDR'])
+	if (isset($_SERVER['REMOTE_ADDR']))
 		Stats::registerVisit();
 
     $config['cms']['admin_cfg'] = require_once $__DIRADM__ . '/../../config/admin.config.php';
