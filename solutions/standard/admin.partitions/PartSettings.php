3af64aca671f45ae1a1b42260d05807d34a17c4c
Student Ivan <cryptox3@gmail.com>
2013-09-03 01:06:20 +0400

    release candidate 17

--- solutions/standard/admin.partitions/PartSettings.php
+++ solutions/standard/admin.partitions/PartSettings.php
@@ -9,6 +9,8 @@ class PartSettings extends PartObjectInterface
 
     public function main()
     {
+        Process::$context['settings'] = json_decode(file_get_contents(dirname(__FILE__) . '/../../../config/dynamical.json'), true);
+        Process::$context['roles_list'] = Roles::listing();
         return Process::getTwigInstance()->render('admin/partitions/configuration.html.twig', Process::$context);
     }
 }
